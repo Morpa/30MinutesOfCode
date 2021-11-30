3292e666e82a607d74dcb371c6bc3d95d0c33ca7
Andre <andre.morpanini@7egend.cr>
2021-11-30 15:51:46 +0000

    Part 1

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from 'react'
+import { http } from './services/api'
 
 function App() {
+  const [users, setUsers] = useState([])
+
+  useEffect(() => {
+    http.get('/users').then(response => setUsers(response.data))
+   }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div>{JSON.stringify(users)}</div>
   );
 }
 
---
+++ src/miragejs/factories/user.js
@@ -0,0 +1,46 @@
+/*
+ * Mirage JS guide on Factories: https://miragejs.com/docs/data-layer/factories
+ */
+import { Factory } from 'miragejs';
+
+/*
+ * Faker Github repository: https://github.com/Marak/Faker.js#readme
+ */
+import faker from 'faker';
+
+export default {
+  user: Factory.extend({
+    name() {
+      return faker.name.findName();
+    },
+    email() {
+      return faker.internet.email();
+    },
+    role() {
+      return faker.random.arrayElement(['admin', 'user']);
+    },
+    department() {
+      return faker.random.arrayElement(['Sales', 'IT', 'Marketing']);
+    },
+    mobile() {
+      return faker.phone.phoneNumber();
+    },
+    address: {
+      street() {
+        return faker.address.streetAddress();
+      },
+      city() {
+        return faker.address.city();
+      },
+      state() {
+        return faker.address.state();
+      },
+      country() {
+        return faker.address.country();
+      },
+      zip() {
+        return faker.address.zipCode();
+      },
+    },
+  }),
+};
